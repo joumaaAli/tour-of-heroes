d6c35780180adb68783de1d05aee7b85da9ec3fb
joumaaAli <ali.joumaa@net.usj.edu.lb>
2024-02-26 21:53:47 +0200

    added list display with selected hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgFor, NgIf],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
